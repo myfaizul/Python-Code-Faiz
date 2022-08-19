bb15ec441f30df01deebbc984a9158dbf1c6ffef
myfaizul <myfaizul@yahoo.com>
2022-08-19 11:54:59 +0530

    Deleted Old Files

--- Python Code Practice 01-Aug-22/single, double, triple quote.py
+++
@@ -1,5 +0,0 @@
-#print('Mother's Love') #here we should not use single quote bcos Mother's have single quote
-print('I like banana')
-print("don't want")
-print(""" my first simple program is "success"
-Let see the output""")
